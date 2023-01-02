301baa16a41061ca8ec8653559c8522a405d70dc
meetparmar160501 <78339815+meetparmar160501@users.noreply.github.com>
2023-01-02 11:45:38 +0530

    Update RNN_LSTM_on_handwritten_digits.py

--- Algorithm_test_code/RNN_LSTM_on_handwritten_digits.py
+++ Algorithm_test_code/RNN_LSTM_on_handwritten_digits.py
@@ -1,47 +1,236 @@
-#from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow.keras.datasets import mnist
-mnist = input_data.read_data_sets("MNIST_data")
 
-n_neurons = 128
-learning_rate = 0.001
-batch_size = 128
-n_epochs = 10
 
-n_steps = 28 # 28 rows
-n_inputs = 28 # 28 cols
-n_outputs = 10 # 10 classes
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
-y = tf.placeholder(tf.int32, [None])
-cell = tf.nn.rnn_cell.BasicRNNCell(num_units=n_neurons)
-output, state = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
-logits = tf.layers.dense(state, n_outputs)
-cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
-loss = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-prediction = tf.nn.in_top_k(logits, y, 1)
-accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+model = keras.Sequential()
+model.add(layers.Embedding(input_dim=1000, output_dim=64))
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data")
-X_test = mnist.test.images # X_test shape: [num_test, 28*28]
-X_test = X_test.reshape([-1, n_steps, n_inputs])
-y_test = mnist.test.labels
+model.add(layers.LSTM(128))
 
-init = tf.global_variables_initializer()
 
-with tf.Session() as sess:
-    sess.run(init)
-    n_batches = mnist.train.num_examples // batch_size
-    for epoch in range(n_epochs):
-        for batch in range(n_batches):
-            X_train, y_train = mnist.train.next_batch(batch_size)
-            X_train = X_train.reshape([-1, n_steps, n_inputs])
-            sess.run(optimizer, feed_dict={X: X_train, y: y_train})
-        loss_train, acc_train = sess.run(
-            [loss, accuracy], feed_dict={X: X_train, y: y_train})
-        print('Epoch: {}, Train Loss: {:.3f}, Train Acc: {:.3f}'.format(
-            epoch + 1, loss_train, acc_train))
-    loss_test, acc_test = sess.run(
-        [loss, accuracy], feed_dict={X: X_test, y: y_test})
-    print('Test Loss: {:.3f}, Test Acc: {:.3f}'.format(loss_test, acc_test))
+model.add(layers.Dense(10))
+
+model.summary()
+
+model = keras.Sequential()
+model.add(layers.Embedding(input_dim=1000, output_dim=64))
+
+model.add(layers.GRU(256, return_sequences=True))
+
+model.add(layers.SimpleRNN(128))
+
+model.add(layers.Dense(10))
+
+model.summary()
+
+encoder_vocab = 1000
+decoder_vocab = 2000
+
+encoder_input = layers.Input(shape=(None,))
+encoder_embedded = layers.Embedding(input_dim=encoder_vocab, output_dim=64)(
+    encoder_input
+)
+
+output, state_h, state_c = layers.LSTM(64, return_state=True, name="encoder")(
+    encoder_embedded
+)
+encoder_state = [state_h, state_c]
+
+decoder_input = layers.Input(shape=(None,))
+decoder_embedded = layers.Embedding(input_dim=decoder_vocab, output_dim=64)(
+    decoder_input
+)
+
+
+decoder_output = layers.LSTM(64, name="decoder")(
+    decoder_embedded, initial_state=encoder_state
+)
+output = layers.Dense(10)(decoder_output)
+
+model = keras.Model([encoder_input, decoder_input], output)
+model.summary()
+
+paragraph1 = np.random.random((20, 10, 50)).astype(np.float32)
+paragraph2 = np.random.random((20, 10, 50)).astype(np.float32)
+paragraph3 = np.random.random((20, 10, 50)).astype(np.float32)
+
+lstm_layer = layers.LSTM(64, stateful=True)
+output = lstm_layer(paragraph1)
+output = lstm_layer(paragraph2)
+output = lstm_layer(paragraph3)
+
+
+lstm_layer.reset_states()
+
+paragraph1 = np.random.random((20, 10, 50)).astype(np.float32)
+paragraph2 = np.random.random((20, 10, 50)).astype(np.float32)
+paragraph3 = np.random.random((20, 10, 50)).astype(np.float32)
+
+lstm_layer = layers.LSTM(64, stateful=True)
+output = lstm_layer(paragraph1)
+output = lstm_layer(paragraph2)
+
+existing_state = lstm_layer.states
+
+new_lstm_layer = layers.LSTM(64)
+new_output = new_lstm_layer(paragraph3, initial_state=existing_state)
+
+model = keras.Sequential()
+
+model.add(
+    layers.Bidirectional(layers.LSTM(64, return_sequences=True), input_shape=(5, 10))
+)
+model.add(layers.Bidirectional(layers.LSTM(32)))
+model.add(layers.Dense(10))
+
+model.summary()
+
+batch_size = 64
+
+input_dim = 28
+
+units = 64
+output_size = 10  # labels are from 0 to 9
+
+
+def build_model(allow_cudnn_kernel=True):
+    
+    if allow_cudnn_kernel:
+        
+        lstm_layer = keras.layers.LSTM(units, input_shape=(None, input_dim))
+    else:
+       
+        lstm_layer = keras.layers.RNN(
+            keras.layers.LSTMCell(units), input_shape=(None, input_dim)
+        )
+    model = keras.models.Sequential(
+        [
+            lstm_layer,
+            keras.layers.BatchNormalization(),
+            keras.layers.Dense(output_size),
+        ]
+    )
+    return model
+
+mnist = keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+sample, sample_label = x_train[0], y_train[0]
+
+model = build_model(allow_cudnn_kernel=True)
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="sgd",
+    metrics=["accuracy"],
+)
+
+
+model.fit(
+    x_train, y_train, validation_data=(x_test, y_test), batch_size=batch_size, epochs=1
+)
+
+noncudnn_model = build_model(allow_cudnn_kernel=False)
+noncudnn_model.set_weights(model.get_weights())
+noncudnn_model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="sgd",
+    metrics=["accuracy"],
+)
+noncudnn_model.fit(
+    x_train, y_train, validation_data=(x_test, y_test), batch_size=batch_size, epochs=1
+)
+
+import matplotlib.pyplot as plt
+
+with tf.device("CPU:0"):
+    cpu_model = build_model(allow_cudnn_kernel=True)
+    cpu_model.set_weights(model.get_weights())
+    result = tf.argmax(cpu_model.predict_on_batch(tf.expand_dims(sample, 0)), axis=1)
+    print(
+        "Predicted result is: %s, target result is: %s" % (result.numpy(), sample_label)
+    )
+    plt.imshow(sample, cmap=plt.get_cmap("gray"))
+
+class NestedCell(keras.layers.Layer):
+    def __init__(self, unit_1, unit_2, unit_3, **kwargs):
+        self.unit_1 = unit_1
+        self.unit_2 = unit_2
+        self.unit_3 = unit_3
+        self.state_size = [tf.TensorShape([unit_1]), tf.TensorShape([unit_2, unit_3])]
+        self.output_size = [tf.TensorShape([unit_1]), tf.TensorShape([unit_2, unit_3])]
+        super(NestedCell, self).__init__(**kwargs)
+
+    def build(self, input_shapes):
+        # expect input_shape to contain 2 items, [(batch, i1), (batch, i2, i3)]
+        i1 = input_shapes[0][1]
+        i2 = input_shapes[1][1]
+        i3 = input_shapes[1][2]
+
+        self.kernel_1 = self.add_weight(
+            shape=(i1, self.unit_1), initializer="uniform", name="kernel_1"
+        )
+        self.kernel_2_3 = self.add_weight(
+            shape=(i2, i3, self.unit_2, self.unit_3),
+            initializer="uniform",
+            name="kernel_2_3",
+        )
+
+    def call(self, inputs, states):
+       
+        input_1, input_2 = tf.nest.flatten(inputs)
+        s1, s2 = states
+
+        output_1 = tf.matmul(input_1, self.kernel_1)
+        output_2_3 = tf.einsum("bij,ijkl->bkl", input_2, self.kernel_2_3)
+        state_1 = s1 + output_1
+        state_2_3 = s2 + output_2_3
+
+        output = (output_1, output_2_3)
+        new_states = (state_1, state_2_3)
+
+        return output, new_states
+
+    def get_config(self):
+        return {"unit_1": self.unit_1, "unit_2": unit_2, "unit_3": self.unit_3}
+
+
+
+unit_1 = 10
+unit_2 = 20
+unit_3 = 30
+
+i1 = 32
+i2 = 64
+i3 = 32
+batch_size = 64
+num_batches = 10
+timestep = 50
+
+cell = NestedCell(unit_1, unit_2, unit_3)
+rnn = keras.layers.RNN(cell)
+
+input_1 = keras.Input((None, i1))
+input_2 = keras.Input((None, i2, i3))
+
+outputs = rnn((input_1, input_2))
+
+model = keras.models.Model([input_1, input_2], outputs)
+
+model.compile(optimizer="adam", loss="mse", metrics=["accuracy"])
+
+
+
+input_1_data = np.random.random((batch_size * num_batches, timestep, i1))
+input_2_data = np.random.random((batch_size * num_batches, timestep, i2, i3))
+target_1_data = np.random.random((batch_size * num_batches, unit_1))
+target_2_data = np.random.random((batch_size * num_batches, unit_2, unit_3))
+input_data = [input_1_data, input_2_data]
+target_data = [target_1_data, target_2_data]
+
+model.fit(input_data, target_data, batch_size=batch_size)
+
