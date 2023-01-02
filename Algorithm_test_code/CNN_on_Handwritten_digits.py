cd56ecfc247a2c2b54b098849c50e423fed93dd4
meetparmar160501 <78339815+meetparmar160501@users.noreply.github.com>
2023-01-02 11:41:00 +0530

    Create CNN_on_Handwritten_digits.py
    testing CNN algorithm on the MNIST dataset 
    here accuracy found was 98%

---
+++ Algorithm_test_code/CNN_on_Handwritten_digits.py
@@ -0,0 +1,34 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+
+(X_train,y_train) , (X_test,y_test)=mnist.load_data()
+
+X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], 1))
+X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],X_test.shape[2],1))
+
+print(X_train.shape)
+print(X_test.shape)
+
+X_train=X_train/255
+X_test=X_test/255
+
+model=Sequential()
+
+model.add(Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)))
+
+model.add(MaxPool2D(2,2))
+
+model.add(Flatten())
+model.add(Dense(100,activation='relu'))
+
+model.add(Dense(10,activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+model.fit(X_train,y_train,epochs=10)
+model.evaluate(X_test,y_test)
